cf9663154f0588f3cb718a46d13f3959afcc6c7e
Stepan Tikunov <stepan.tikunov@niuitmo.ru>
2023-12-11 08:09:45 +0300

    Rewrite

--- main.js
+++
@@ -1,3 +0,0 @@
-    document.getElementById("telegram").addEventListener("change", function(){
-    document.getElementById('tglink').innerHTML = "<a href='"+this.value + "'>"+this.value+"</a>";
-    });
